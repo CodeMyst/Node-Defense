3f5ae25b0d0f0d08244fda15c3945cc56f344428
LeonLaci <leonlaci@outlook.com>
2017-01-14 13:10:43 +0100

    Added more comments Removed System.Collections

--- Assets/Scripts/leaderboardMenu.cs
+++ Assets/Scripts/leaderboardMenu.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 using System.Collections.Generic;
 
 public class leaderboardMenu : MonoBehaviour {
@@ -8,37 +7,43 @@ public class leaderboardMenu : MonoBehaviour {
 	public Text leaderBoardText;
 	public int maxScoresToDisplay;
 
+    // Get the list of all scores from the leaderboard
 	List<dreamloLeaderBoard.Score> scoreList;
-
 
 	dreamloLeaderBoard leaderboard;
 	dreamloPromoCode pc;
 
+    // Used for initialization
 	void Start ()
 	{
 		// get the reference here...
 		this.leaderboard = dreamloLeaderBoard.GetSceneDreamloLeaderboard ();
 
-		leaderboard.AddScore ("mmmmmmmmmm", 0); //Need to do this or the leaderboard won't show... wtf
+		leaderboard.AddScore ("mmmmmmmmmm", 0); //Need to do this or the leaderboard won't show... weird
 	}
 	
 	// Called once per frame
 	void OnGUI () {
+        // Order the score list from high to low scores
 		scoreList = leaderboard.ToListHighToLow ();
 
-		if (scoreList == null || scoreList.Count == 0) {
+        // If there aren't any scores yet display a message
+		if (scoreList == null || scoreList.Count == 0)
+        {
 			leaderBoardText.text = "No Scores Yet!";
-		} else {
+		}
+        else
+        {
+            // If there are scores, display them
+		    int count = 0;
+		    leaderBoardText.text = "";
+		    foreach (dreamloLeaderBoard.Score score in scoreList) {
+			    count++;
 
-			int count = 0;
-			leaderBoardText.text = "";
-			foreach (dreamloLeaderBoard.Score score in scoreList) {
-				count++;
+			    leaderBoardText.text += score.playerName + " - Wave:" + score.score + "\n";
 
-				leaderBoardText.text += score.playerName + " - Wave:" + score.score + "\n";
-
-				if(count >= maxScoresToDisplay) break;
-			}
+			    if(count >= maxScoresToDisplay) break;
+		    }
 		}
 	}
 }
