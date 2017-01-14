a3ff1149aa729a6ecb0f75757aa7fc0f2a6cc5da
LeonLaci <leonlaci@outlook.com>
2017-01-14 13:02:26 +0100

    Added comments Check the TODO list in this script

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -1,26 +1,30 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class gameManager : MonoBehaviour {
 
-	public GameObject gameoverUI;
+	public GameObject gameoverUI;       // Store the gameOverUI object so we can disable it or enable it
 	public GameObject yes, no, toMenu;
 	[Header("Speed")]
+    // Buttons for allowing the player to increase or decrease the time scale
 	public GameObject speedUp;
 	public GameObject slowDown;
 
 	[HideInInspector]
-	public static bool gameEnded;
+	public static bool gameEnded;       // Check if the game has ended
 
-	dreamloLeaderBoard leaderboard;
+	dreamloLeaderBoard leaderboard;     // Get the leaderboard script
 
+    // Used for initialization (called before Start())
 	void Awake(){
 		Application.runInBackground = true; //Keep the game playing even if it loses focus
 
-		gameEnded = false; //Need to do this on scene start or static variable will be preserved
+        // Need to do this on scene start or static variable will be preserved
+        // since the static variables stay loaded even after scene load
+        gameEnded = false;
 		gameObject.GetComponent<activeBuffs> ().applyNodeBuffs(); //Apply node buffs at start
 	}
 
+    // Used for initialization (called after Awake())
 	void Start(){
 		this.leaderboard = dreamloLeaderBoard.GetSceneDreamloLeaderboard ();
 		//playerStats.current.printBuffs ();
@@ -31,6 +35,9 @@ public class gameManager : MonoBehaviour {
 	void Update () {
 		//Check framerate
 		//Debug.Log (Mathf.FloorToInt(1.0f / Time.deltaTime));
+
+        // If the game has ended exit the Update method
+        // so it wont do anything
 		if (gameEnded)
 			return;
 
@@ -39,17 +46,19 @@ public class gameManager : MonoBehaviour {
 			playerStats.current.addExpPerDiff (5000);
 		}
 
-
+        // If the player's lives reach 0 then end the game
 		if (gameStats.lives <= 0) {
 			endGame ();
 		}
 	}
 
+    // Used for ending the game
 	void endGame(){
-		waveSpawner.enemiesAlive = 0;
-		gameEnded = true;
-		gameoverUI.SetActive(true);
+		waveSpawner.enemiesAlive = 0;       // Set the enemies alive to 0
+		gameEnded = true;                   // Set the game ended to true
+		gameoverUI.SetActive(true);         // Enable the game over screen
 
+        // Add experience to the player
 		playerStats.current.addExpPerDiff (waveSpawner.getLastWave());
 		playerStats.current.setLevel ();
 
@@ -57,6 +66,7 @@ public class gameManager : MonoBehaviour {
 		if (playerStats.saveIndex >= 0)
 			SaveLoad.Save (playerStats.saveIndex);
 
+        // Add the score to the leaderboard
 		leaderboard.AddScore (playerStats.current.saveName, waveSpawner.getLastWave ());
 	}
 		
@@ -68,25 +78,27 @@ public class gameManager : MonoBehaviour {
 		toMenu.SetActive (false);
 	}
 
-	//Close in the even that the user does not want to go to the menu
+	//Close in the event that the user does not want to go to the menu
 	public void closeConfirmation(){
 		yes.SetActive (false);
 		no.SetActive (false);
 		toMenu.SetActive (true);
 	}
 
+    // Used for increasing the time scale (speed at which the game is running)
 	public void incSpeed(){
 		speedUp.SetActive (false);
 		slowDown.SetActive (true);
+        // Speed the game 3 time
 		Time.timeScale *= 3f;
 	}
 
+    // Used for decreasing the time scale
 	public void decSpeed(){
 		speedUp.SetActive (true);
 		slowDown.SetActive (false);
+        // Slow the game 3 times to the starting speed
+        // TODO: Change this from /= 3f to only 1f?
 		Time.timeScale /= 3f;
 	}
-		
-
-
 }
